baef24e90a8367a69178a229dfc36c866c2d800f
Ngonidzashe Mangudya <imngonii@gmail.com>
2025-05-17 12:47:14 +0200

    feat: ✨ Add initial project setup docs: 📚 Add initial README chore: 🔧 Add analysis options build: 📦 Add build configurations feat: ✨ Add flavors to project feat: ✨ Add basic counter app test: 🧪 Add counter cubit tests style: 💄 Update formatting chore: 🔧 Add VSCode launch configurations chore: 🔧 Add dependabot configuration chore: 🔧 Add cspell configuration ci: 👷 Add main workflow

---
+++ android/app/src/main/kotlin/za/codecraftsolutions/pingbox/MainActivity.kt
@@ -0,0 +1,6 @@
+package za.codecraftsolutions.pingbox
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:pingbox/app/app.dart';
+import 'package:pingbox/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
